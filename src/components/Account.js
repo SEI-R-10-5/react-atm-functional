fffd33fb87911580c56e3f42b4133c631e84f53e
Andre Pato <anpato1994@gmail.com>
2020-11-19 15:53:44 -0500

    add solution

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,40 +1,51 @@
 import React, { useState } from 'react'
 
-const Account = props => {
-    let [amount, setAmount] = useState(0)
-    let [balance, setBalance] = useState(0)
+const Account = (props) => {
+  let [amount, setAmount] = useState(0)
+  let [balance, setBalance] = useState(0)
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        if (isNaN(amount)) {
-            console.log("Not a number")
-        }
-        else {
-            setBalance(balance + Number(amount))
-        }
-        setAmount(0)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (isNaN(amount)) {
+      console.log('Not a number')
+      return
     }
-
-    let balanceClass = 'balance'
-    if (balance <= 0) {
-        balanceClass += ' zero'
+    const actionType = e.target.value
+    switch (actionType) {
+      case 'Withdraw':
+        amount > balance
+          ? console.log('Nah')
+          : setBalance(balance - Math.abs(Number(amount)))
+        return setAmount(0)
+      case 'Deposit':
+        setBalance(balance + Math.abs(Number(amount)))
+        return setAmount(0)
+      default:
+        return
     }
+  }
 
-    return (
-        <div className="account">
-            <h2>{props.name}</h2>
-            <div className={balanceClass}>${balance}</div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="enter an amount" 
-                    value={amount} 
-                    onChange={ e => setAmount(e.target.value) }
-                />
-                <input type="submit" value="Deposit" />
-            </form>
-        </div>
-    )
+  let balanceClass = 'balance'
+  if (balance <= 0) {
+    balanceClass += ' zero'
+  }
+
+  return (
+    <div className="account">
+      <h2>{props.name}</h2>
+      <div className={balanceClass}>${balance}</div>
+      <form>
+        <input
+          type="text"
+          placeholder="enter an amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input type="submit" value="Deposit" onClick={handleSubmit} />
+        <input type="submit" value="Withdraw" onClick={handleSubmit} />
+      </form>
+    </div>
+  )
 }
 
 export default Account
